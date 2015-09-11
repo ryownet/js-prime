cf8167eb2b50f7a9b0e0ce99d4e631e168fdeae2
ryownet <ryownet@gmail.com>
2015-09-12 01:04:17 +0900

    Sieve of Eratosthenes alog added

--- prime.js
+++ prime.js
@@ -11,41 +11,39 @@ var Main = function( data ){
     console.log ('not valid number');
     return false;
   }
-  for(var i=2; i< inputNum; i++){
-    if( isPrime(i, result) ){
-      result.push(i);
+
+  var tansakuList = new Array(inputNum);
+
+  // 探索リストの中身をすべて素数として仮置き
+  // 0, 1は素数でないので除外
+  for (var i = 0; i < inputNum; i++) {
+    tansakuList[i] = true;
+  }
+  tansakuList[0] = false;
+  tansakuList[1] = false;
+  // 最大値の平方根
+  var max = Math.floor(Math.sqrt(inputNum));
+
+  // 探索リストの先頭 >= inputNumの平方根まで素数リストの倍数(2x)をふるい落とす
+  // 2で割ってあまりを求めるのではなく、2をかけたものを全部消す
+  for(var i = 2; i <= max; i++) {
+    if(tansakuList[i]) {
+        for(var j=i*2; j <= inputNum; j += i) {
+            tansakuList[j] = false;
+        }
     }
   }
+
+//素数の出力
+for(var i = 0; i < inputNum; i++) {
+    if(tansakuList[i] === true) {
+        result.push(i);
+    }
+}
+
   console.log(input + 'in primes:', result.length);
   if( debug ) console.timeEnd('t');
 }
-
-// param {int} n, {array} primeArray
-// return boolean
-var isPrime = function( n, primeArray ){
-  var l = primeArray.length;
-  var currentMax = 1;
-  if( l > 0 ){
-    if( n in primeArray) return false;
-    for(var i=0; i<l; i++){
-      if( n % primeArray[i] === 0 ) return false;
-    }
-    //currentMax = Math.max.apply(null, primeArray);
-    currentMax = primeArray[l-1];
-  }
-  for(var j=currentMax+1; j<n-1; j++){
-    if( n % j === 0 ){
-      return false;
-    }
-  }
-  return true;
-};
-
-/* 考え方
-素数配列で割れれば素数、割なければ素数配列の最大値からnまでをインクリメントして割り切れるかチェック
-*/
-
-
 
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
